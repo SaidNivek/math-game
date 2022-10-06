9b18ca8181f264924407fb7d2b0cf43bdd42e6ef
SaidNivek <kevin.vi.dias@gmail.com>
2022-10-06 16:55:51 -0400

    Moved the notes

--- src/game-screen-components/GameScreen.jsx
+++ src/game-screen-components/GameScreen.jsx
@@ -9,11 +9,14 @@ function GameScreen() {
 
   return (
     <>
-        <GameInfo />
-        <Functions />
-        <Numbers />
-        <CurrentMath />
-        <Submit />
+      <GameInfo />
+      <Functions />
+      <Numbers />
+      <CurrentMath />
+      <Submit />
+      <p>When a unique correct answer is achieved, the game will play a jingle and/or animation, then add the variation to the # correct tracker, then reset.</p>
+      <p>Do we want to have a maximum # correct that will display to the user when they've reached them ALL or allow them to go forever until the time runs out or they hit 'Submit'?</p>
+      <p>It seems strange that the player could get all the answers and NOT be told they've got the correct answers.</p>
     </>
   )
 }
